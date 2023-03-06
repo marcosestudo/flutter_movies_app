c474aec2dd3b18a8f29422f6f808fb77ecff07ee
marcos <estudogit@outlook.com>
2023-03-06 20:29:59 -0300

    home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart'; 
-import 'package:get/get.dart'; 
+import 'package:get/get.dart';
+import 'package:primeiro_app/app/routes/app_pages.dart';
 
 void main() {
   runApp(GetMaterialApp(   
     debugShowCheckedModeBanner: true,   
-    initialRoute: '/',
+    initialRoute: Routes.HOME,
     defaultTransition: Transition.fade,   
-    getPages: MyRoutes.routes,
+    getPages: AppPages.pages,
   ));   
 }

cf157f5fa0def2fc169121189ad86eaf4afcfaa5
marcos <estudogit@outlook.com>
2023-03-06 20:51:08 -0300

    arquivo home_page.dart reescrito

--- lib/app/ui/home/home_page.dart
+++ lib/app/ui/home/home_page.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:http/http.dart' as http;
-class MyPage extends StatelessWidget {
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('My Page')),
-      body: Container(
-        child: GetX<MyController>(
-            init: MyController(),
-            builder: (_) {
-              return Container();
-            }),
-      ),
+      appBar: AppBar(title: Text('Home Page')),
+      body: GetBuilder<HomeController>(
+          init: HomeController(),
+          builder: (_) {
+            return Container();
+          }),
     );
   }
 }

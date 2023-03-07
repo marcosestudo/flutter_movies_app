d6df756b19d7dcdc611b0779b1de81d1350c3d4b
marcos <estudogit@outlook.com>
2023-03-06 21:09:03 -0300

    home page

--- lib/app/ui/home/home_page.dart
+++ lib/app/ui/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
+import 'package:primeiro_app/app/controllers/home_controller.dart';
+// note que no terceiro import não se coloca a pasta lib, já vai direto pra pasta app
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -8,11 +9,11 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home Page')),
+      appBar: AppBar(title: const Text('Home Page')),
       body: GetBuilder<HomeController>(
           init: HomeController(),
           builder: (_) {
-            return Container();
+            return const Center(child: Text("Essa é a nossa home page (｡•́‿•̀｡)"));
           }),
     );
   }

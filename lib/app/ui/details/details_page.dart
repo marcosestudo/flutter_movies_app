8658d60705c40e65d41a317a14cc373b7e70a8e4
marcos <estudogit@outlook.com>
2023-03-07 14:48:53 -0300

    wrap whit GeX

--- lib/app/ui/details/details_page.dart
+++ lib/app/ui/details/details_page.dart
@@ -8,13 +8,22 @@ class DetailsPage extends GetView<DetailsController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Details Page (｡•́‿•̀｡)'), centerTitle: true, automaticallyImplyLeading: true),
+      appBar: AppBar(title: const Text('Details Page (｡•́‿•̀｡)'),
+          centerTitle: true,
+          automaticallyImplyLeading: true),
       body: GetBuilder<DetailsController>(
-          init: DetailsController(),
+          // init: DetailsController(), foi pro arquivo details_binding
+          // details_controller deve ser iniciado antes da página para ser visto pelo wrap Getx
+          // por isso saiu daqui e foi pra binding
           builder: (_) {
-            return const Center(child: Text("essa é a nossa detail page"));
+            return Center(child: GetX<DetailsController>(
+              builder: (_) {
+                return Text("${controller.number}", style: const TextStyle(fontSize: 22));
+              },
+            ));
           }),
-      floatingActionButton: FloatingActionButton(onPressed: controller.onPressedFloatingAction),
+      floatingActionButton: FloatingActionButton(
+          onPressed: controller.onPressedFloatingAction),
     );
   }
 }

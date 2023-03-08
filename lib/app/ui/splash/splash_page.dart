ab7d7cbb6cdb83b9f07230691dcec835329ae6a9
marcos <estudogit@outlook.com>
2023-03-08 18:59:01 -0300

    adicionado splashcontroller.dart a lib/app/controllers

--- lib/app/ui/splash/splash_page.dart
+++ lib/app/ui/splash/splash_page.dart
@@ -82,7 +82,7 @@ class SplashPage extends StatelessWidget {
                     ),
                   ),
                 ),
-              ),
+              )
             ],
           );
         },

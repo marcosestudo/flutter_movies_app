e4df0f9017b2e69dbd7406aa99bc9cc4fe9c7efa
marcos <estudogit@outlook.com>
2023-03-08 19:35:30 -0300

    metodo onReady em splash_screen.dart

--- lib/app/controllers/splash_controller.dart
+++ lib/app/controllers/splash_controller.dart
@@ -1,9 +1,14 @@
 import 'package:get/get.dart';
-import 'package:meta/meta.dart';
+import 'package:primeiro_app/app/routes/app_pages.dart';
 
-class MyController extends GetxController {
+class SplashController extends GetxController {
+  bool visibleStrip = false;
+  double animatedMargin = 0.0;
 
-  final _obj = ''.obs;
-  set obj(value) => _obj.value = value;
-  get obj => _obj.value;
+  @override
+  void onReady() {
+    visibleStrip = true;
+    animatedMargin = 80.0;
+    update();
+  }
 }
--- lib/app/ui/splash/splash_page.dart
+++ lib/app/ui/splash/splash_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/src/simple/get_state.dart';
+import 'package:primeiro_app/app/controllers/splash_controller.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);

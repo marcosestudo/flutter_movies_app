b3ef712463176e6c70e3d07a7fbdae86c6e3ac31
marcos <estudogit@outlook.com>
2023-03-13 18:15:25 -0300

    provider

---
+++ lib/app/data/repository/movie_repository.dart
@@ -0,0 +1,12 @@
+import 'package:meta/meta.dart';
+import 'package:primeiro_app/app/data/providers/api.dart';
+
+class MovieRepository {
+  final MovieApiClient? apiClient;
+
+  MovieRepository({@required this.apiClient}) : assert(apiClient != null);
+
+  Future getAll() {
+    return apiClient!.getAll();
+  }
+}

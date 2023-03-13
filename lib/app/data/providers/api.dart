6d07fc719b3d0cac371fa4ad86e91b7aaad442a9
marcos <estudogit@outlook.com>
2023-03-13 18:06:06 -0300

    api.dart

---
+++ lib/app/data/providers/api.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+import 'package:meta/meta.dart';
+import 'package:primeiro_app/app/data/model/movie_model.dart';
+
+const baseUrl = 'https://script.google.com/a/macros/id.uff.br/s/AKfycbwGSu1psIFPpLeIco4C0rgaYPOv4yjgfpV0fuA6l0xMGCFqdREcIj1or_QtalqtYP_tVw/exec';
+
+class MovieApiClient {
+  final http.Client? httpClient;
+
+  MovieApiClient({@required this.httpClient});
+
+  Future<List<MovieModel>> getAll() async {
+    try {
+      final response = await httpClient!.get(Uri.parse(baseUrl));
+      if (response.statusCode == 200) {
+        List<dynamic> jsonResponse = jsonDecode(response.body);
+        return jsonResponse.map((movieJson) => MovieModel.fromJson(movieJson)).toList();
+      } else {
+        debugPrint('Error -getAll');
+      }
+    } catch (e) {
+      debugPrint("Error fetcing from API: $e");
+    }
+    return [];
+  }
+}

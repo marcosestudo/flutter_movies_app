16d3f1d2fa0243d0a74517b1dde3b754c1ae530f
marcos <estudogit@outlook.com>
2023-03-14 20:18:49 -0300

    tentando visualizar nome do diretor

--- lib/app/ui/home/home_page.dart
+++ lib/app/ui/home/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:primeiro_app/app/controllers/home_controller.dart';
-import 'package:primeiro_app/app/data/model/movie_model.dart';
 // note que no terceiro import não se coloca a pasta lib, já vai direto pra pasta app
 
 // extends StatelessWidget trocado por extends GetView
@@ -11,30 +10,16 @@ class HomePage extends GetView<HomeController> {
 
   @override
   Widget build(BuildContext context) {
-    MovieModel movieInfo = Get.arguments["movie_info"];
     return Scaffold(
-      appBar: AppBar(
-          title: const Text('Home Page (｡•́‿•̀｡)'),
-          centerTitle: true,
-          // actions são os botões que ficam no canto superior direito da appbar
-          actions: [
-            IconButton(
-                onPressed: controller.reloadData,
-                icon: const Icon(Icons.refresh))
-          ]),
-      body: GetBuilder<HomeController>(builder: (_) {
-        return Column(
-          children: [
-            Center(
-                child: controller.isLoading
-                    ? const CircularProgressIndicator()
-                    : ElevatedButton(
-                        onPressed: _.onPressed,
-                        child: const Text("Counter Page"))),
-            Text(movieInfo.titulo),
-          ],
-        );
-      }),
+      backgroundColor: Colors.black,
+      appBar: AppBar(title: const Text('Filmes nacionais'), centerTitle: true, actions: [IconButton(onPressed: controller.reloadData, icon: const Icon(Icons.refresh))],),
+      body: GetBuilder<HomeController>(
+          builder: (_) {
+            return controller.isLoading ? const Center(child: CircularProgressIndicator()) :
+            Text(
+                controller.movieList[0].diretor
+            );
+          }),
     );
   }
 }

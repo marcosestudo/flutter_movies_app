c9535685bf1c299894e1dad2cd5c8a43eb6b9d0b
marcos <estudogit@outlook.com>
2023-03-15 16:38:41 -0300

    bindings desnescessarias removidas

--- lib/app/bindings/details_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:primeiro_app/app/controllers/details_controller.dart';
- 
-class DetailsBinding extends Bindings { 
-  @override 
-  void dependencies() { 
-    Get.put(DetailsController());
-  }
-}

27f44cc69de4d868f7d5ab66c0c4d9fd3c8f5582
marcos <estudogit@outlook.com>
2023-03-15 17:37:50 -0300

    home controller criado e home binding dependenscies atualizadas

--- lib/app/bindings/home_binding.dart
+++ lib/app/bindings/home_binding.dart
@@ -1,9 +1,12 @@
 import 'package:get/get.dart';
 import 'package:primeiro_app/app/controllers/home_controller.dart';
+import 'package:primeiro_app/app/data/providers/api.dart';
+import 'package:primeiro_app/app/data/repository/movie_repository.dart';
+import 'package:http/http.dart' as http;
 
 class HomeBinding extends Bindings {
   @override
   void dependencies() {
-    Get.put(HomeController());
+    Get.put(HomeController(movieRepository: MovieRepository(apiClient: MovieApiClient(httpClient: http.Client()))));
   } 
 }
--- lib/app/controllers/home_controller.dart
+++ lib/app/controllers/home_controller.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:primeiro_app/app/data/repository/movie_repository.dart';
 
 class HomeController extends GetxController {
 
+    final MovieRepository? movieRepository;
+    HomeController({@required this.movieRepository}) : assert(movieRepository != null);
 }

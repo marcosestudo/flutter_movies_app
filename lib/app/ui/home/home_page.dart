08dbf988c941d558a66e486c6a880d18bdaad6cc
marcos <estudogit@outlook.com>
2023-03-15 18:19:40 -0300

    home page atualizada, falta definir os metodos no controller

--- lib/app/ui/home/home_page.dart
+++ lib/app/ui/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:primeiro_app/app/controllers/home_controller.dart';
+import 'package:vertical_card_pager/vertical_card_pager.dart';
 
 
 class HomePage extends GetView<HomeController> {
@@ -10,7 +11,13 @@ class HomePage extends GetView<HomeController> {
       appBar: AppBar(title: Text('Home Page')),
       body: GetBuilder<HomeController>(
           builder: (_) {
-            return Container();
+            return controller.isLoading! ? const Center(child: CircularProgressIndicator()) :
+                VerticalCardPager(
+                  intilaPage: 1,
+                  titles: const <String>["","","",""],
+                  images: controller.images,
+                  onSelectedItem: controller.onSelectedItem,
+                );
           }),
     );
   }

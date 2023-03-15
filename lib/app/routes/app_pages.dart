de556ec93014b01f2616b6a90b26ac90fe431ddd
marcos <estudogit@outlook.com>
2023-03-15 16:36:38 -0300

    app_pages e app_routes atualizados

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,7 +1,4 @@
 import 'package:get/get.dart';
-import 'package:primeiro_app/app/bindings/details_binding.dart';
-import 'package:primeiro_app/app/bindings/home_binding.dart';
-import 'package:primeiro_app/app/ui/details/details_page.dart';
 import 'package:primeiro_app/app/ui/home/home_page.dart';
 import 'package:primeiro_app/app/ui/splash/splash_page.dart';
 // note que no import não se coloca a pasta lib, já vai direto pra pasta app
@@ -12,7 +9,6 @@ abstract class AppPages{
 
   static final pages = [
     GetPage(name: Routes.SPLASH, page: () => SplashPage()),
-    GetPage(name: Routes.HOME, page: () => HomePage(), binding: HomeBinding()),
-    GetPage(name: Routes.DETAILS, page: () => DetailsPage(), binding: DetailsBinding())
+    GetPage(name: Routes.HOME, page: () => HomePage()),
   ];
 }

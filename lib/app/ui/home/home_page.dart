392f12598b3931d58c86f22181a1938cb14b8fa1
marcos <estudogit@outlook.com>
2023-03-15 16:52:01 -0300

    home_controller vazio criado

--- lib/app/ui/home/home_page.dart
+++ lib/app/ui/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:primeiro_app/app/controllers/home_controller.dart';
 
 
 class HomePage extends GetView<HomeController> {

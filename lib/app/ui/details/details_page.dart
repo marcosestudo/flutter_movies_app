d591cddc7a47ec3af54137ef422c4684ced14dac
marcos <estudogit@outlook.com>
2023-03-15 23:15:59 -0300

    details page adicionada

--- lib/app/ui/details/details_page.dart
+++ lib/app/ui/details/details_page.dart
@@ -1,31 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:primeiro_app/app/controllers/details_controller.dart';
 
-class DetailsPage extends GetView<CounterController> {
-  const DetailsPage({Key? key}) : super(key: key);
+class DetailsPage extends StatelessWidget {
+  const DetailsPage({Key? key}): super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Counter Page (｡•́‿•̀｡)'),
-          centerTitle: true,
-          automaticallyImplyLeading: true),
-      body: GetBuilder<CounterController>(
-          // init: DetailsController(), foi pro arquivo details_binding
-          // details_controller deve ser iniciado antes da página para ser visto pelo wrap Getx
-          // por isso saiu daqui e foi pra binding
+      appBar: AppBar(title: Text('Details Page')),
+      body: GetX<DetailsController>(
+          init: DetailsController(),
           builder: (_) {
-            return Center(child: GetX<CounterController>(
-              builder: (_) {
-                return Text("${controller.number}", style: const TextStyle(fontSize: 22));
-              },
-            ));
+            return Container();
           }),
-      floatingActionButton: FloatingActionButton(
-          onPressed: controller.onPressedFloatingAction,
-          child: const Icon(Icons.add),
-      )
     );
   }
 }

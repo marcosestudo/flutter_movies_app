d21f43664188337bc9b7c333ba722a0f4cb6ae4a
marcos <estudogit@outlook.com>
2023-03-17 12:56:39 -0300

    iniciando implementacao da funcao edit() no provider

--- lib/app/data/providers/api.dart
+++ lib/app/data/providers/api.dart
@@ -25,4 +25,18 @@ class MovieApiClient {
     }
     return [];
   }
+
+  Future edit(obj) async {
+    try {
+      final response = await httpClient!.put(Uri.parse(baseUrl),
+          headers: _defaultHeaders, body: jsonEncode(obj));
+      if (response.statusCode == 200) {
+        // TODO: implement methods!
+      } else {
+        print('Error -edit');
+      }
+    } catch (_) {}
+    return null;
+  }
+
 }

137dc564d1b054d1e0222a4421df1adbd6fd2b01
marcos <estudogit@outlook.com>
2023-03-18 15:55:03 -0300

    metodo onpressedLikeBtn atualizado

--- lib/app/controllers/details_controller.dart
+++ lib/app/controllers/details_controller.dart
@@ -5,17 +5,26 @@ import 'package:primeiro_app/app/controllers/home_controller.dart';
 
 class DetailsController extends GetxController {
   final MovieRepository? movieRepository;
+  bool loadingLike = false;
 
   DetailsController({@required this.movieRepository}) : assert(movieRepository != null);
+
+  final index = Get.find<HomeController>().onPressedIndex;
 
   final _likedMovie = Get.find<HomeController>().liked.obs;
   get likedMovie => _likedMovie.value;
   set likedMovie(value) => _likedMovie.value = value;
 
-  final index = Get.find<HomeController>().onPressedIndex;
+
 
   void onPressedLikeBtn() {
     likedMovie = !likedMovie;
-    movieRepository?.likedEdit(index);
+
+    loadingLike = true;
+    update();
+    movieRepository?.likedEdit(index).then((value) {
+      loadingLike = false;
+      update();
+    });
   }
 }
